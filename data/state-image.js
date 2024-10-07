9bc38fdcc40ed3052c015723a85d24aacd5e4c90
erfan taheri <erfanta197@gmail.com>
2024-10-07 15:51:10 +0330

    add initial data we need

---
+++ data/state-image.js
@@ -0,0 +1,10 @@
+const stateImage = {
+  universal:
+    "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/icons/hero_universal.png",
+  strength:
+    "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/icons/hero_strength.png",
+  intelligence:
+    "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/icons/hero_intelligence.png",
+  agility:
+    "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/icons/hero_agility.png",
+};
