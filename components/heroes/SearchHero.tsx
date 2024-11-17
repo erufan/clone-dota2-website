c827812008214860c22e2b97edc6b1192be10aac
erfan taheri <erfanta197@gmail.com>
2024-11-17 21:38:26 +0330

    fix bug value of search hero will gone if you refresh or came from out side of the site

--- components/heroes/SearchHero.tsx
+++ components/heroes/SearchHero.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import useHandleQuery from "@/hooks/useHandleQuery";
+import { useSearchParams } from "next/navigation";
 import { FaSearch } from "react-icons/fa";
 
 const SearchHero = () => {
   const handleQuery = useHandleQuery("name");
+  const param = useSearchParams();
 
   return (
     <>
@@ -13,6 +15,7 @@ const SearchHero = () => {
           className="bg-[#25282A] p-1 outline-0 focus:border-[none] focus:bg-[#505050]"
           type="text"
           onChange={(e) => handleQuery(e.target.value)}
+          value={param?.get("name") || ""}
         />
       </form>
       <FaSearch className="ml-1" color="#4D4D4D" size={24} />
